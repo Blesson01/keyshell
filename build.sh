165ddf2a2ceb76195f9faf8ff557611bf9216084
Blesson <blezzzblesson8@gmail.com>
2022-04-18 18:48:19 +0530

    build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Branch : ${GIT_BRANCH}"
+
+
+echo "Installing Dependencies...."
+npm install || exit 1
+
+echo "Removing old builds"
+rm -rvf dist/*
+
+echo "Building Applictions"
+ng build --base-href || exit 1
+
+echo "Dropping old apps from EC2"
+ssh bznode@54.166.151.240 'rm -rvf /var/www/keyshel/*'
+
+echo "Rsync build"
+rsynx avzP dist/keyshell/* bznode@54.166.151.240:/var/www/keyshel/"
+
+if [ $? = 0 ]; then
+	echo "Deployed the App Successfully"
+	fi
