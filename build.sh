9edc93f8385eba3b741dc1d5b8f1435375c11932
Blesson <blezzzblesson8@gmail.com>
2022-04-18 20:55:52 +0530

    build.sh 8

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ echo "Branch : ${GIT_BRANCH}"
 echo "Installing Dependencies...."
 #npm install || exit 1
 
-#echo "Removing old builds"
-#rm -rvf dist/*
+echo "Removing old builds"
+rm -rvf dist/*
 
-echo "ng install"
+#echo "ng install"
 #npm install -g @angular/cli
 
 echo "Building Applictions"
 ng build --base-href / || exit 1
 
-#echo "Dropping old apps from EC2"
-#ssh bznode@54.166.151.240 'rm -rvf /var/www/keyshel/*'
+echo "Dropping old apps from EC2"
+ssh bznode@54.166.151.240 'rm -rvf /var/www/keyshel/*'
 
 echo "Rsync build"
 rsync -avzP dist/keyshell/* bznode@54.166.151.240:/var/www/keyshel/"
